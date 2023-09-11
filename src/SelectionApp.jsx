7f9263e7b81a74fbb15aa7a3b7a43b4f4ff121ee
Hammad Ur Rehman <141633644+DevelopWithHammad@users.noreply.github.com>
2023-09-11 13:11:21 +0500

    React - Assignment 2

---
+++ src/SelectionApp.jsx
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+import "./style.css"
+
+function SelectionApp() {
+
+    const [myCounter, setMyCounter] = useState(1);
+
+    const [message, setMessage] = useState([
+        "Learn React",
+        "Now Have Handsome Income:)",
+        "Invest Your New Income:D",
+    ])
+
+
+    function nextHandler() {
+        console.log("nextHandler is running!");
+
+        if (myCounter < 3) {
+            setMyCounter(myCounter + 1)
+        }
+
+    }
+
+
+
+    function previousHandler() {
+        console.log("previousHandler is running!");
+
+        if (myCounter >= 1) {
+            setMyCounter(myCounter - 1)
+        }
+    }
+
+
+    return (
+        <>
+            <div style={{ display: "flex", justifyContent: "space-around", marginTop: "200px"}}>
+
+
+                {
+                    Array.from([1, 2, 3], container => (
+                        <div style={{ height: '50px', width: '50px', borderRadius: '50%', backgroundColor: myCounter >= container ? "orange" : "gray", textAlign: 'center', color: 'white', display: 'flex', alignItems: 'center', justifyContent: 'center', marginTop: "-80px" }}>{container}</div>
+                    ))
+                }
+
+            </div>
+
+            <h1 style={{ textAlign: 'center',  marginTop: "60px"}}>
+                {
+                    message[myCounter - 1]
+                }
+            </h1>
+            <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                {
+                    Array.from(["previous", "next"], btn => (
+                        <button className='btns' onClick={btn == "previous" ? previousHandler : nextHandler} style={{ border: "2px solid black", width: "300px", height: "60px", marginTop: "100px", borderRadius: "100px", cursor: "pointer" }}>{btn}</button>
+                    ))
+                }
+            </div>
+
+
+
+
+        </>
+    )
+
+
+}
+
+
+export default SelectionApp
+
+
+
+// import { useState } from "react";
+
+// function App() {
+
+// }
